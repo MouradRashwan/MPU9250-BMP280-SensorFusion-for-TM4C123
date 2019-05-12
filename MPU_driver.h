d3da6dcda5d9aeeec2026f8f12f5d1367d7f2dd9
Mourad Rashwan <32272179+MouradRashwan@users.noreply.github.com>
2019-05-12 20:12:41 +0200

    Update MPU_driver.h

--- MPU_driver.h
+++ MPU_driver.h
@@ -8,8 +8,8 @@
 #ifndef MPU_DRIVER_H_
 #define MPU_DRIVER_H_
 
-#include "MCAL_layer/COMMON_driver/COMMON_driver.h"
-#include "MCAL_layer/I2C_driver/I2C_driver.h"
+#include "COMMON_driver/COMMON_driver.h"
+#include "I2C_driver/I2C_driver.h"
 
 #include "MPU_config.h"
 #include "MPU_defs.h"
